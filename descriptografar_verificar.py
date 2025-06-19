0fdfce577f2fb67d01d82194923a6269eb28e98c
Erik Silva <134088430+ErikDSilva@users.noreply.github.com>
2025-06-19 01:10:16 +0000

    Descriptografando

--- descriptografar_verificar.py
+++ descriptografar_verificar.py
@@ -1,5 +1,5 @@
-# descriptografar_verificar.py
 import rsa
+import base64
 
 # Carregar chave privada da Equipe B
 with open('chaves/chave_privada_b.pem', 'rb') as f:
@@ -9,9 +9,10 @@ with open('chaves/chave_privada_b.pem', 'rb') as f:
 with open('chaves/chave_publica_a.pem', 'rb') as f:
     public_key_a = rsa.PublicKey.load_pkcs1(f.read())
 
-# Carregar mensagem criptografada
+# Carregar mensagem criptografada em base64
 with open('mensagens/mensagem_cripto.txt', 'rb') as f:
-    mensagem_cripto = f.read()
+    mensagem_cripto_base64 = f.read()
+    mensagem_cripto = base64.b64decode(mensagem_cripto_base64)
 
 # Descriptografar com a chave privada da Equipe B
 mensagem = rsa.decrypt(mensagem_cripto, private_key_b)
--- gerar_chaves_b.py
+++ gerar_chaves_b.py
@@ -1,10 +1,10 @@
 # gerar_chaves_b.py
 import rsa
 
-public_key, private_key = rsa.newkeys(2048)
+# public_key, private_key = rsa.newkeys(2048)
 
-with open('chaves/chave_publica_b.pem', 'wb') as pub:
-    pub.write(public_key.save_pkcs1())
+# with open('chaves/chave_publica_b.pem', 'wb') as pub:
+#     pub.write(public_key.save_pkcs1())
 
-with open('chaves/chave_privada_b.pem', 'wb') as priv:
-    priv.write(private_key.save_pkcs1())
+# with open('chaves/chave_privada_b.pem', 'wb') as priv:
+#     priv.write(private_key.save_pkcs1())
